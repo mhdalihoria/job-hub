3f6db0591ff746bf41ad27caa40e0016cbf2d09e
mhdalihoria <alihoria123456@gmail.com>
2023-10-18 09:28:21 +0300

    Adding Details on the document header

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="description" content="Job Hub is a service that provides you with easy access to high quality jobs to get recruited quickly and easily. And for you, as an employer, to find the best candidates in the shortest amount of time possible" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <body>
         <Main />
         <NextScript />
