c81d07a48787a45c34f75dd317fe99b57a2c1d71
mhdalihoria <alihoria123456@gmail.com>
2023-10-20 00:06:47 +0300

    Adding Google Font

--- pages/_document.js
+++ pages/_document.js
@@ -1,17 +1,25 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-import Navbar from "@/components/navbar/Navbar"
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="description" content="Job Hub is a service that provides you with easy access to high quality jobs to get recruited quickly and easily. And for you, as an employer, to find the best candidates in the shortest amount of time possible" />
+        <meta
+          name="description"
+          content="Job Hub is a service that provides you with easy access to high quality jobs to get recruited quickly and easily. And for you, as an employer, to find the best candidates in the shortest amount of time possible"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
