aa5a0179549001bed24b5155a84f6ee4e5ee83c7
mhdalihoria <alihoria123456@gmail.com>
2023-10-20 00:09:07 +0300

    - Adding Sticky Footer to Layout - Adding Header Component

--- layouts/DefaultLayout.jsx
+++ layouts/DefaultLayout.jsx
@@ -1,11 +1,28 @@
+import { Box } from "@mui/material";
+import Footer from "@/components/footer/Footer";
 import Navbar from "@/components/navbar/Navbar";
+import Head from "next/head";
+
+const containerStyle = {
+  display: "flex",
+  flexDirection: "column",
+  minHeight: "100vh",
+};
+
+const contentStyle = {
+  flex: 1,
+};
 
 function Layout({ children }) {
   return (
-    <div>
+    <Box sx={containerStyle}>
       <Navbar />
-      {children}
-    </div>
+      <Head>
+        <title>Job Hub</title>
+      </Head>
+      <Box sx={contentStyle}>{children}</Box>
+      <Footer />
+    </Box>
   );
 }
 
