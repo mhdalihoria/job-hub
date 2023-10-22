1f548090dc66362e8254c88ff87eae148ce3db99
mhdalihoria <alihoria123456@gmail.com>
2023-10-22 12:18:58 +0300

    Toggling dark/light theme using Zustand's store

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import { ThemeProvider } from "@mui/material";
 import { lightTheme, darkTheme } from "@/styles/theme";
 import "@/styles/globals.css";
+import useThemeStore from "@/stores/themeStore";
 
 export default function App({ Component, pageProps }) {
-  const theme = lightTheme;
+  const { isLightTheme } = useThemeStore();
+  const theme = isLightTheme ? lightTheme : darkTheme;
+
   return (
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
   );
 }
