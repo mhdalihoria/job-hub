e078979b426cefa198db5793c9797fc4ff418cbe
mhdalihoria <alihoria123456@gmail.com>
2023-10-26 23:40:17 +0200

    Changing the store so it can hold more data/properties

--- stores/userStore.js
+++ stores/userStore.js
@@ -10,6 +10,9 @@ const useUserStore = create((set) => ({
     set((state) => ({
       userCredentials: { ...state.userCredentials, firstName, lastName, id },
     })),
+    //---------------------!! For now the above code is for user credentials name, and id. but it will be saved in local and fire store storage and it will be replaced with the code below
+    userData: null, 
+    setUserData: (data) => set({userData: data})
 }));
 
 export default useUserStore;
