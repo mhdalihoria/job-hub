59f31a9b266d1f6b69a28f5671ec10801f1c0c76
mhdalihoria <alihoria123456@gmail.com>
2023-10-29 11:34:08 +0200

    logging userData state at all time

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,11 +2,14 @@ import { ThemeProvider } from "@mui/material";
 import { lightTheme, darkTheme } from "@/styles/theme";
 import "@/styles/globals.css";
 import useThemeStore from "@/stores/themeStore";
+import useUserStore from "@/stores/userStore";
 
 export default function App({ Component, pageProps }) {
   const { isLightTheme } = useThemeStore();
   const theme = isLightTheme ? lightTheme : darkTheme;
-
+  const { userData } = useUserStore();
+  console.log(userData);
+  
   return (
     <ThemeProvider theme={theme}>
       <Component {...pageProps} />
