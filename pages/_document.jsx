a49205065f203074ded033058e165f044b7da741
mhdalihoria <alihoria123456@gmail.com>
2023-11-01 15:49:41 +0200

    Fixing Warning

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -8,7 +8,7 @@ export default function Document() {
           name="description"
           content="Job Hub is a service that provides you with easy access to high quality jobs to get recruited quickly and easily. And for you, as an employer, to find the best candidates in the shortest amount of time possible"
         />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta content="width=device-width, initial-scale=1" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
