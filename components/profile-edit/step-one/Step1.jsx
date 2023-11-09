b057eab8a2bc833e58796c14f99629743a6f31e8
mhdalihoria <alihoria123456@gmail.com>
2023-11-09 22:21:31 +0200

    - Changing User Type from "Select" into "Buttons"
    - Changing "links" from one text field into 2 fields (1 select, 1 textField). With the ability to increment and decrement
    
    - Refactoring (and seperating)

--- components/profile-edit/step-one/Step1.jsx
+++ components/profile-edit/step-one/Step1.jsx
@@ -1,43 +1,63 @@
 import React, { useState } from "react";
-import { ErrorMessage, Field, Form, Formik } from "formik";
+import { ErrorMessage, Field, FieldArray, Form, Formik, getIn } from "formik";
 import * as Yup from "yup";
 import {
   Button,
   ButtonBase,
   FormControl,
+  FormHelperText,
   Input,
   InputAdornment,
   InputLabel,
   MenuItem,
   Select,
+  TextField,
   Typography,
   styled,
 } from "@mui/material";
 import defaultUser from "@/public/imgs/default-user.png";
 import Image from "next/image";
-
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import LanguageIcon from "@mui/icons-material/Language";
+//----------------------------------------------------------
 const MAX_FILE_SIZE = 819200; //800KB
 
-const validFileExtensions = { profileImage: ["jpg", "png", "jpeg", "webp"] };
+const phoneRegExp =
+  /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
 
-function isValidFileType(fileName, fileType) {
-  return (
-    fileName &&
-    validFileExtensions[fileType].indexOf(fileName.split(".").pop()) > -1
-  );
-}
 const validationSchema = Yup.object().shape({
   userType: Yup.string().required("What type of user are you?"),
-  phoneNum: Yup.string().required("Phone Number is required"),
+  phoneNum: Yup.string()
+    .required("Phone number is required")
+    .matches(phoneRegExp, "Phone number is not valid"),
   location: Yup.string().required("Location is required"),
-  links: Yup.string().required("Please Provide your Link(s)"),
-  profileImage: Yup.mixed().required("Required"),
-  // .test(
-  //   "is-valid-size",
-  //   "Max allowed size is 800KB",
-  //   (value) => value && value.size <= MAX_FILE_SIZE
-  // ),
+  // links: Yup.string().required("Please Provide your Link(s)"),
+  socialMedia: Yup.array().of(
+    Yup.object().shape({
+      platform: Yup.string().required("Select a social media platform"),
+      value: Yup.string().required("Enter a value"),
+    })
+  ),
+  profileImage: Yup.mixed()
+    .required("Required")
+    .test(
+      "is-valid-size",
+      "Max allowed size is 800KB",
+      (value) => value && value.size <= MAX_FILE_SIZE
+    ),
 });
+
+const initialValues = {
+  userType: "",
+  phoneNum: "",
+  location: "",
+  // links: "",
+  socialMedia: [{ platform: "", value: "" }],
+  profileImage: null,
+};
+
+const platforms = ["github", "linkedin", "website"];
 
 const StepOne = ({
   activeStep,
@@ -72,68 +92,71 @@ const StepOne = ({
     handleNext();
   };
 
+  const showSocialIcon = (platform) => {
+    switch (platform) {
+      case "github": {
+        return <GitHubIcon />;
+      }
+      case "linkedin": {
+        return <LinkedInIcon />;
+      }
+      case "website": {
+        return <LanguageIcon />;
+      }
+      default:
+        return null;
+    }
+  };
+
   return (
     <Formik
-      initialValues={{
-        userType: "",
-        phoneNum: "",
-        location: "",
-        links: "",
-        profileImage: null,
-      }}
+      initialValues={initialValues}
       validationSchema={validationSchema}
       onSubmit={handleSubmit}
     >
       {({ setFieldValue, handleBlur, values, touched, errors }) => (
         <Form>
           <div>
-            <Field
-              name="userType"
-              as={FormControl}
-              variant="outlined"
-              fullWidth
-            >
-              <InputLabel>Who Are You?</InputLabel>
-              <Field
-                as={Select}
-                name="userType"
-                id="userType"
-                variant="standard"
-              >
-                <MenuItem
-                  value="jobSeeker"
-                  id="jobSeekerOption"
-                  name="jobSeekerOption"
-                >
-                  Job Seeker
-                </MenuItem>
-                <MenuItem
-                  value="employer"
-                  id="employerOption"
-                  name="employerOption"
-                >
-                  Employer
-                </MenuItem>
-              </Field>
+            <Field name="userType" variant="outlined" fullWidth>
+              {({ field, form }) => {
+                return (
+                  <>
+                    <InputLabel>Who Are You?</InputLabel>
+                    <Button
+                      {...field}
+                      onClick={() =>
+                        form.setFieldValue("userType", "jobseeker")
+                      }
+                    >
+                      Job Seeker
+                    </Button>
+                    <Button
+                      {...field}
+                      onClick={() => form.setFieldValue("userType", "employer")}
+                    >
+                      Employer
+                    </Button>
+                  </>
+                );
+              }}
             </Field>
-
             <ErrorMessage name="userType" component="div" />
           </div>
 
           <div>
-            <Field
-              name="phoneNum"
-              as={FormControl}
-              variant="outlined"
-              fullWidth
-            >
-              <InputLabel>Phone Number</InputLabel>
-              <Field
-                as={Input}
-                name="phoneNum"
-                id="phoneNum"
-                variant="standard"
-              ></Field>
+            <Field name="phoneNum" variant="outlined" fullWidth>
+              {({ form, field }) => (
+                <>
+                  {/* <InputLabel>Phone Number</InputLabel> */}
+                  <TextField
+                    id="phoneNum"
+                    variant="standard"
+                    label={"Phone Number"}
+                    {...field}
+                    fullWidth
+                  />
+                </>
+              )}
             </Field>
             <ErrorMessage name="phoneNum" component="div" />
           </div>
@@ -156,6 +179,7 @@ const StepOne = ({
             <ErrorMessage name="location" component="div" />
           </div>
 
+          {/* links
           <div>
             <Field name="links" as={FormControl} variant="outlined" fullWidth>
               <InputLabel>Social Links (Website, LinkedIn, etc...) </InputLabel>
@@ -167,7 +191,81 @@ const StepOne = ({
               ></Field>
             </Field>
             <ErrorMessage name="links" component="div" />
-          </div>
+          </div> */}
+
+          <FieldArray name="socialMedia">
+            {({ push, remove }) => (
+              <>
+                {values.socialMedia.map((_, index) => (
+                  <div key={index}>
+                    <Field
+                      name={`socialMedia.${index}.platform`}
+                      validateOnChange={false}
+                    >
+                      {({ field, form }) => (
+                        <FormControl
+                          error={
+                            getIn(
+                              form.errors,
+                              `socialMedia.${index}.platform`
+                            ) &&
+                            getIn(form.touched, `socialMedia.${index}.platform`)
+                          }
+                        >
+                          <Select {...field} label="Social Media Platform">
+                            {platforms.map((platform) => (
+                              <MenuItem key={platform} value={platform}>
+                                {showSocialIcon(platform)}
+                              </MenuItem>
+                            ))}
+                          </Select>
+                          <ErrorMessage
+                            name={`socialMedia.${index}.platform`}
+                            component={FormHelperText}
+                          />
+                        </FormControl>
+                      )}
+                    </Field>
+                    <Field
+                      name={`socialMedia.${index}.value`}
+                      validateOnChange={false}
+                    >
+                      {({ field, form }) => (
+                        <FormControl
+                          error={
+                            getIn(form.errors, `socialMedia.${index}.value`) &&
+                            getIn(form.touched, `socialMedia.${index}.value`)
+                          }
+                        >
+                          <TextField {...field} label="Value" />
+                          <ErrorMessage
+                            name={`socialMedia.${index}.value`}
+                            component={FormHelperText}
+                          />
+                        </FormControl>
+                      )}
+                    </Field>
+                    {index > 0 && (
+                      <Button
+                        type="button"
+                        onClick={() => {
+                          remove(index);
+                        }}
+                      >
+                        Remove
+                      </Button>
+                    )}
+                  </div>
+                ))}
+                <Button
+                  type="button"
+                  onClick={() => push({ platform: "", value: "" })}
+                >
+                  Add Field
+                </Button>
+              </>
+            )}
+          </FieldArray>
 
           <div>
             <Field name="profileImage">
@@ -202,8 +300,8 @@ const StepOne = ({
                         src={imagePreview}
                         alt="Preview"
                         style={{ maxWidth: "100%", marginTop: "10px" }}
-                        width={50}
-                        height={50}
+                        width={100}
+                        height={100}
                       />
                     )}
                   </div>
