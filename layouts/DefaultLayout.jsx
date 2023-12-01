63508e3fa7d1485c8f73343b5e3c846a5851f6db
mhdalihoria <alihoria123456@gmail.com>
2023-12-01 19:51:54 +0200

    Fixing Bug (on very large screen sizes dark backgrounds wouldn't cover entire page between navbar and footer)

--- layouts/DefaultLayout.jsx
+++ layouts/DefaultLayout.jsx
@@ -1,16 +1,17 @@
-import { Box } from "@mui/material";
+import { Box, styled } from "@mui/material";
 import Footer from "@/components/footer/Footer";
 import Navbar from "@/components/navbar/Navbar";
 import Head from "next/head";
+
+const ContentStyled = styled(Box)(({ theme }) => ({
+  flex: 1,
+  background: theme.palette.background.paper,
+}));
 
 const containerStyle = {
   display: "flex",
   flexDirection: "column",
   minHeight: "100vh",
-};
-
-const contentStyle = {
-  flex: 1,
 };
 
 function Layout({ children }) {
@@ -20,7 +21,7 @@ function Layout({ children }) {
       <Head>
         <title>Job Hub</title>
       </Head>
-      <Box sx={contentStyle}>{children}</Box>
+      <ContentStyled>{children}</ContentStyled>
       <Footer />
     </Box>
   );
