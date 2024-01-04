1ef1a6f9d8dc5e35c3131608b6c21985901bfaad
mhdalihoria <alihoria123456@gmail.com>
2024-01-04 21:04:12 +0200

    Passing userData to Children

--- pages/profile/index.jsx
+++ pages/profile/index.jsx
@@ -4,15 +4,18 @@ import useUserStore from "@/stores/userStore";
 import EmployerProfile from "@/page-sections/profile/EmployerProfile";
 import SeekerProfile from "@/page-sections/profile/SeekerProfile";
 
+//--------------------------------------------------------------
+//--------------------------------------------------------------
+
 const Profile = () => {
   const { userData } = useUserStore();
 
   return (
     <DefaultLayout>
       {!!userData && userData.userType === "seeker" ? (
-        <SeekerProfile />
+        <SeekerProfile userData={userData}/>
       ) : (
-        <EmployerProfile />
+        <EmployerProfile userData={userData}/>
       )}
     </DefaultLayout>
   );
