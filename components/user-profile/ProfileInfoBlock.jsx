fc9c7c9c62fcd2ef03e20e6333cfe6c73af9b435
mhdalihoria <alihoria123456@gmail.com>
2024-01-04 17:07:06 +0200

    Creating Dynamic Boilerplate for Generic Info Block

---
+++ components/user-profile/ProfileInfoBlock.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import {
+  Card,
+  styled,
+  Container,
+  Box,
+  Avatar,
+  IconButton,
+  Grid,
+  Paper,
+  Stack,
+  Typography,
+  useMediaQuery,
+  Divider,
+} from "@mui/material";
+
+//--------------------------------------------------------------
+//--------------------------------------------------------------
+
+const SectionContainer = styled(Card)(({ theme }) => ({
+  padding: "2rem",
+}));
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
+
+//--------------------------------------------------------------
+//--------------------------------------------------------------
+
+const data = [
+  {
+    endDate: "Sat, 30 Dec 2023 21:00:00 GMT",
+    description:
+      "Course Descriptionn\nIn publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.",
+    startDate: "Fri, 29 Dec 2023 21:00:00 GMT",
+    subTitle: "Course Author",
+    title: "Course Title",
+  },
+];
+
+const ProfileInfoBlock = () => {
+  const formatTime = (date) => {
+    const startDate = new Date(date);
+    return startDate.toLocaleDateString("en-GB");
+  };
+
+  return (
+    <SectionContainer>
+      <Container maxWidth="lg">
+        <Typography variant="h5" gutterBottom mb={4}>
+          Section Title
+        </Typography>
+        {data.map((item, index) => {
+          return (
+            <Box key={item.title}>
+              <Typography component="h1" variant="button" gutterBottom>
+                <strong>{item.title}</strong>
+              </Typography>
+              <Typography component="h2" variant="body2" gutterBottom>
+                {item.subTitle}
+              </Typography>
+              <Typography component="p" variant="caption" gutterBottom>
+                {formatTime(item.startDate)} - {formatTime(item.endDate)}
+              </Typography>
+              <Typography component="p" variant="body1" gutterBottom>
+                {item.description}
+              </Typography>
+              {data.length > 0 && index !== data.length - 1 && (
+                <Divider sx={{ m: "1.5rem 0" }} />
+              )}
+            </Box>
+          );
+        })}
+      </Container>
+    </SectionContainer>
+  );
+};
+
+export default ProfileInfoBlock;
