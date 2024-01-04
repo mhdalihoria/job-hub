54913aa3ebfdcb40b7524e60128175d2ad74efc2
mhdalihoria <alihoria123456@gmail.com>
2024-01-04 21:03:38 +0200

    - Rearranging Data and Passing it to Relevant Profile Blocks as Props (w/ filtering out falsy values)

--- page-sections/profile/SeekerProfile.jsx
+++ page-sections/profile/SeekerProfile.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, styled } from "@mui/material";
+import { Grid, styled } from "@mui/material";
 import ProfileIntro from "@/components/user-profile/ProfileIntro";
 import ProfileInfoBlock from "@/components/user-profile/ProfileInfoBlock";
 import ProfileSkillsBlock from "@/components/user-profile/ProfileSkillsBlock";
@@ -16,6 +16,7 @@ const ProfileContainer = styled("div")(({ theme }) => ({
 //--------------------------------------------------------------
 
 const SeekerProfile = ({ userData }) => {
+  // Profile Intro Arrays (start) ------------------------------
   const userIntroduction = [
     { title: "First Name", value: userData.firstName },
     { title: "Last Name", value: userData.lastName },
@@ -45,6 +46,33 @@ const SeekerProfile = ({ userData }) => {
       link: userData.reseme,
     },
   ].filter(Boolean);
+  // Profile Intro Arrays (end) ----------------------------------
+
+  // User Courses Array (start) ----------------------------------
+  const userCourses =
+    userData.courses?.map((item) => ({
+      startDate: item.startDate,
+      endDate: item.endDate,
+      title: item.courseTitle,
+      description: item.courseDescription,
+      subTitle: item.courseAuthor,
+    })) ?? [];
+  // User Courses Array (end)  ------------------------------------
+
+  // User Work Exp Array (start) ----------------------------------
+  const userWorkExp =
+    userData.workExp?.map((item) => ({
+      startDate: item.startDate,
+      endDate: item.endDate,
+      title: item.jobTitle,
+      description: item.jobRole,
+      subTitle: item.companyName,
+    })) ?? [];
+  // User Courses Array (end)  -----------------------------------
+
+  // User Skills Array (start)  ----------------------------------
+  const userSkills = userData.skills.length > 0 ? userData.skills : [];
+  // User Skills Array (end)   -----------------------------------
 
   return (
     <ProfileContainer>
@@ -54,8 +82,25 @@ const SeekerProfile = ({ userData }) => {
         userJobTitle={`${userData.jobTitle}`}
         userLinks={userLinks}
       />
-      <ProfileInfoBlock />
-      <ProfileSkillsBlock />
+      <Grid container columnSpacing={2} rowSpacing={2}>
+        <Grid item xs={12} sm={12} md={8}>
+          {userCourses && (
+            <ProfileInfoBlock
+              sectionTitle={"Courses"}
+              sectionData={userCourses}
+            />
+          )}
+          {userWorkExp && (
+            <ProfileInfoBlock
+              sectionTitle={"Work Experience"}
+              sectionData={userWorkExp}
+            />
+          )}
+        </Grid>
+        <Grid item xs={12} sm={12} md={4}>
+          {userSkills && <ProfileSkillsBlock userSkills={userSkills} />}
+        </Grid>
+      </Grid>
     </ProfileContainer>
   );
 };
